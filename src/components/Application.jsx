caf557b5e55e0db859e8d6813cc066308ca02bb6
lucky <lucky.hw.kim@gmail.com>
2022-04-20 18:27:13 -0700

    Add axios to fetch data from api

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,25 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = {
   1: {
@@ -62,6 +46,16 @@ const appointments = {
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(()=>{
+    const daysUrl = '/api/days';
+    axios.get(daysUrl).then(response => {
+      setDays([...response.data])
+      console.log(...response.data);
+    })
+  },[])
+  
 
   return (
     <main className="layout">
@@ -73,7 +67,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-        <DayList days={days} value={day} onChange={setDay} />
+        <DayList days={days} value={days} onChange={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
