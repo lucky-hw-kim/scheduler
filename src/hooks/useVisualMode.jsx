50da1a0e58fb9f8e5e636f533ff1c2eba470478f
lucky <lucky.hw.kim@gmail.com>
2022-04-21 19:16:29 -0700

    Add custom hooks for appointment navigation

---
+++ src/hooks/useVisualMode.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+export function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  function transition(newMode, replace = false) {
+    if(replace) {
+    setHistory((prev) => [...prev])
+    setMode(newMode);
+    } else {
+    setMode(newMode);
+    setHistory((prev) => [...prev, newMode]);
+    }
+  }
+
+  function back() {
+    if(history.length > 1){
+    history.pop();
+    setMode(history[history.length - 1]);
+    } else {
+      setMode(initial);
+    }
+  }
+
+  return { mode, transition, back };
+}
